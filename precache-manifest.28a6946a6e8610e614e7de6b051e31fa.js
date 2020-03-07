74d9e26f332c628823f08ae51c09cf97a6c1a1b7
Lim Jing Jie <xiaojie96@xiaojies-MacBook-Pro.local>
2020-03-07 12:46:36 +0800

    Updates

---
+++ precache-manifest.28a6946a6e8610e614e7de6b051e31fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7664d226981523315a394aa99fbbb64a",
+    "url": "/gh-test/index.html"
+  },
+  {
+    "revision": "f7d6f362561634a572e6",
+    "url": "/gh-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "772eb94d315ed2895e32",
+    "url": "/gh-test/static/js/2.16742486.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gh-test/static/js/2.16742486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7d6f362561634a572e6",
+    "url": "/gh-test/static/js/main.8940f7e2.chunk.js"
+  },
+  {
+    "revision": "e7ed847dae974b2265ec",
+    "url": "/gh-test/static/js/runtime-main.1521e0f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
